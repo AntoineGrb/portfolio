47d187e9aabffe8e3da12acaa7b36c250dd799e6
Antoine Grubert <antoine_grubert@hotmail.fr>
2023-10-17 16:30:59 +0200

    animations js en cours - reste l'apparition des projets

--- src/js/script.js
+++ src/js/script.js
@@ -13,7 +13,105 @@ function cursorWinking() {
     }
 }
 
-let interval = setInterval(cursorWinking , 500);
+setInterval(cursorWinking , 500);
 
 //Gérer l'écriture du titre du header
+const typingLetters = [
+    {letter:'M', interval:'1200'},
+    {letter:'y', interval:'1400'},
+    {letter:'\u00A0', interval:'1600'},
+    {letter:'P', interval:'1800'},
+    {letter:'o', interval:'1900'},
+    {letter:'r', interval:'2000'},
+    {letter:'t', interval:'2100'},
+    {letter:'f', interval:'2200'},
+    {letter:'o', interval:'2300'},
+    {letter:'l', interval:'2400'},
+    {letter:'i', interval:'2600'},
+    {letter:'o', interval:'2800'},
+];
+const headerText = document.querySelector('#header-text');
+
+function writeHeaderTitle() {
+    for (let el of typingLetters) {
+        setTimeout(() => {
+            headerText.innerText += el.letter
+        }, el.interval);
+    }
+}
+
+function eraseHeaderTitle() {
+    for (let el of typingLetters) {
+        setTimeout(() => {
+            headerText.innerText = headerText.innerText.substring(0, headerText.innerText.length - 1);
+        }, el.interval);
+    }
+}
+
+function handleHeaderTitle() {
+    headerText.innerText = '';
+
+    writeHeaderTitle();
+
+    setTimeout(() => {
+        eraseHeaderTitle()
+    }, 15000); 
+}
+
+handleHeaderTitle();
+setInterval(handleHeaderTitle, 20000);
+
+//Gérer le hover des cards projets
+const projectCells = document.querySelectorAll('.project__cell');
+
+projectCells.forEach((cell) => {
+    cell.addEventListener('mouseover' , () => {
+        const card = cell.firstElementChild;
+        card.style.opacity = '1';
+        card.style.visibility = 'visible';
+    })
+});
+
+projectCells.forEach((cell) => {
+    cell.addEventListener('mouseout' , () => {
+        const card = cell.firstElementChild;
+        card.style.opacity = '0';
+        card.style.visibility = 'hidden';
+    })
+});
+
+//Gérer le scroll de la page
+const homeSection = document.querySelector('#home');
+const projectsSection = document.querySelector('#projects');
+const skillsSection = document.querySelector('#skills');
+
+const navLink = document.querySelectorAll('.nav__link');
+const navLinkHome = document.querySelector('.nav__link--home');
+const navLinkProjects = document.querySelector('.nav__link--projects');
+const navLinkSkills = document.querySelector('.nav__link--skills');
+
+function handleMenuNavigation(navElement) {
+    navLink.forEach((el) => {
+        el.classList.remove('active');
+    });
+    navElement.classList.add('active');
+}
+
+window.addEventListener('scroll' , () => {
+
+    let homePosition = homeSection.getBoundingClientRect();
+    let projectsPosition = projectsSection.getBoundingClientRect();
+    let skillsPosition = skillsSection.getBoundingClientRect();
+
+    if (skillsPosition.top < 300) {
+        handleMenuNavigation(navLinkSkills);
+    }
+    else if (projectsPosition.top < 300) {
+        handleMenuNavigation(navLinkProjects);
+    }
+    else {
+        handleMenuNavigation(navLinkHome);
+    }   
+});
+
 
